0bfe250f50bc910de4fcaf92bc7da8abeb9ef8d5
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-13 20:09:14 +0500

    inital fetch completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+// Access key
+// qi_g9Ep6XHt-2FuHXeq5NTyRsqhM1iDuqLdO2u_p0jM
+// sceret key
+// AtH_z3xOKNmbDXV_wCCOcnFScOMkT7wjDp6mXj31jUQ
 
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, SetLoading] = useState(false);
+  const [photos,SetPhotos]=useState([])
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}?client_id=qi_g9Ep6XHt-2FuHXeq5NTyRsqhM1iDuqLdO2u_p0jM`;
+    try {
+      SetLoading(true)
+      const respose = await fetch(url);
+      const data = await respose.json();
+      SetPhotos(data);
+      SetLoading(false)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  return <h2>stock photos starter</h2>;
 }
 
-export default App
+export default App;
